d1d5bc658ea3cbdca66ee5f97dd7a4fc2ab224ad
Laveder Mark-Ylann <mark-ylann.laveder@etu.umontpellier.fr>
2020-05-28 14:05:18 +0200

    Ajout de la liste d'activités

---
+++ src/app/liste-activites/liste-activites.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListeActivitesComponent } from './liste-activites.component';
+
+describe('ListeActivitesComponent', () => {
+  let component: ListeActivitesComponent;
+  let fixture: ComponentFixture<ListeActivitesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ListeActivitesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListeActivitesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/liste-activites/liste-activites.component.ts
@@ -0,0 +1,58 @@
+import {Component, OnInit} from '@angular/core';
+import {CalendarService} from '../services/calendar.service';
+import {Subscription} from 'rxjs';
+import {newArray} from '@angular/compiler/src/util';
+import {CustomEvents} from '../modules/CustomEvents';
+
+@Component({
+  selector: 'app-liste-activites',
+  templateUrl: './liste-activites.component.html',
+  styleUrls: ['./liste-activites.component.css']
+})
+export class ListeActivitesComponent implements OnInit {
+
+  eventsSub: Subscription;
+
+  events: any[];
+  sorted: any[];
+  filtered: any[];
+
+  filterTerm = '';
+
+  ngOnInit() {
+    this.eventsSub = this.calendarService.eventsSubject.subscribe(
+      (events: any[]) => {
+        this.events = events;
+        this.sorted = this.sortEvents();
+        this.searchData();
+      }
+    );
+    this.calendarService.emitEventsSubject();
+
+  }
+
+  constructor(private calendarService: CalendarService) {
+  }
+
+  sortEvents() {
+    const keys = ['title', 'category'];
+    return this.events.filter(
+      (s => o =>
+          (k => !s.has(k) && s.add(k))
+          (keys.map(k => o[k]).join('|'))
+      )
+        // tslint:disable-next-line:new-parens
+      (new Set)
+    );
+  }
+
+  searchData() {
+    this.filtered = this.sorted.filter((event: CustomEvents) => {
+      return event.title.toLowerCase().includes(this.filterTerm.toLowerCase());
+    });
+  }
+
+  sidenavAddEvent(titre, duree, couleur, categorie) {
+    this.calendarService.sidenavAddEvent(titre, duree, couleur, categorie);
+  }
+}
